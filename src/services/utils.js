7459dcd96e899b441677dd718adb50306afa0175
Cristian Meoño <cristianfrancisco85@gmail.com>
2021-11-29 18:30:16 -0600

    [ADD] Ultimo commit

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,4 +1,4 @@
-export const URL_SERVER = 'http://localhost:4000'
+export const URL_SERVER = 'http://ec2-3-15-156-30.us-east-2.compute.amazonaws.com:4000'
 export const URL_S3 = 'https://semi1p2-13.s3.us-east-2.amazonaws.com/'
 
 export const encodeBase64 = async (myblob) =>{
